8afd963812e203283872321ae14d7e2e40228b72
Achraf Souk <48942933+achrafsouk@users.noreply.github.com>
2024-09-27 06:42:44 +0400

    Update image-optimization-stack.ts

--- lib/image-optimization-stack.ts
+++ lib/image-optimization-stack.ts
@@ -208,12 +208,12 @@ export class ImageOptimizationStack extends Stack {
     var imageDeliveryCacheBehaviorConfig: ImageDeliveryCacheBehaviorConfig = {
       origin: imageOrigin,
       viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      compress: false,
       cachePolicy: new cloudfront.CachePolicy(this, `ImageCachePolicy${this.node.addr}`, {
         defaultTtl: Duration.hours(24),
         maxTtl: Duration.days(365),
         minTtl: Duration.seconds(0),
-        queryStringBehavior: cloudfront.CacheQueryStringBehavior.all(),
-        compress: false
+        queryStringBehavior: cloudfront.CacheQueryStringBehavior.all()
       }),
       functionAssociations: [{
         eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
